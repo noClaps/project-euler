b78d2bb2867cb42bb0b9cf6e1611e90677416dec
noClaps <github@zerolimits.dev>
2023-10-27 01:16:44 +0100

    Rename 3-alt.py to 3.py and format primes.py

--- python/3.py
+++ python/3.py
@@ -1,12 +1,12 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
-
-divisor = 2
+from primes import primes
 num = 600851475143
-while divisor <= num:
-    if num % divisor == 0:
-        num /= divisor
-    else: divisor += 1
+for prime in primes:
+    if num == prime:
+        break
+    if num % prime == 0:
+        num /= prime
 
-print(divisor)
+print(num)
 # 6857
