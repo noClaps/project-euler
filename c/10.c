bdcc8390992bb26e3d0ca012d0adf6e0d5860b62
noClaps <github@zerolimits.dev>
2023-10-28 02:12:58 +0100

    Problem 10 solution in C

---
+++ c/10.c
@@ -0,0 +1,31 @@
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+// Find the sum of all the primes below two million.
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  int limit = 2000000;
+  int sievebound = (limit - 1) / 2;
+  int sieve[limit];
+  int crosslimit = (floor(sqrt(limit)) - 1) / 2;
+  for (int i = 1; i <= crosslimit; i++) {
+    if (sieve[i] != 1) {
+      for (int j = 2 * i * (i + 1); j <= sievebound; j += 2 * i + 1) {
+        sieve[j] = 1;
+      }
+    }
+  }
+
+  long sum = 2;
+  for (int i = 1; i <= sievebound; i++) {
+    if (sieve[i] != 1) {
+      sum += 2 * i + 1;
+    }
+  }
+
+  printf("%ld\n", sum);
+  // 142913828922
+
+  return 0;
+}
