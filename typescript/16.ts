f6d08cd2d340d735607022450cbd9cfbf70af970
noClaps <github@zerolimits.dev>
2023-11-23 13:10:13 +0000

    Use sumOfArray function from `lib` in P16 TS

--- typescript/16.ts
+++ typescript/16.ts
@@ -1,19 +1,12 @@
 // 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 // What is the sum of the digits of the number 2^1000? 
 
-function sum(arr: number[]) {
-    let sum = 0;
-    for (const val of arr) {
-        sum += val;
-    }
-
-    return sum;
-}
+import { sumOfArray } from "./lib";
 
 const myNumStr = (2n ** 1000n).toString();
 const numArr = myNumStr.split("").map(n => parseInt(n));
 
-console.log(sum(numArr));
+console.log(sumOfArray(numArr));
 // 1366
 
 export { };
