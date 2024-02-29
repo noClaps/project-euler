7753fa13f73c2ccbcf1e9576a09446f8aa6278a9
noClaps <github@zerolimits.dev>
2024-02-29 01:59:22 +0000

    Remove TypeScript
    So many of the notes won't make sense now, it's gonna be great

--- typescript/16.ts
+++
@@ -1,9 +0,0 @@
-import { sumOfArray } from "./lib";
-
-const myNumStr = (2n ** 1000n).toString();
-const numArr = myNumStr.split("").map((n) => parseInt(n));
-
-console.log(sumOfArray(numArr));
-// 1366
-
-export {};
